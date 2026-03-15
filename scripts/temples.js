dfd81baeb6f81127c21ab4458bf66b24905077ee
Luciano Siqueira Barboza <lbarboza@byupathway.edu>
2026-03-15 10:39:37 -0300

    sandwich menu inclusion

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,3 +3,18 @@ const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const menuBtn = document.querySelector("#menuBtn");
+const navMenu = document.querySelector("#navMenu");
+
+menuBtn.addEventListener("click", () => {
+
+    navMenu.classList.toggle("open");
+
+    if (navMenu.classList.contains("open")) {
+        menuBtn.textContent = "✖";
+    } else {
+        menuBtn.textContent = "☰";
+    }
+
+});
