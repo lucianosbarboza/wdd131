2b4e7ca551e4d4d7d00eb49de1ec94a8d8c2c786
Luciano Siqueira Barboza <lbarboza@byupathway.edu>
2026-03-17 15:40:10 -0300

    finished week02 exercises

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,11 +1,26 @@
-const input = document.querySelector("#favchap");
-const button = document.querySelector('button');
-const list = document.querySelector('_______');
 
-const li = document.createELement('li');
+const inputElement = document.querySelector("#favchap");
+const buttonElement = document.querySelector("button");
+const listElement = document.querySelector("#list");
 
-const deleteButton = document.createElement('button');
-li.textContent = input.value; 
-deleteButton.textContent = 'X';
-li.append(deleteButton);
-list.append(li);
+buttonElement.addEventListener("click", function () {
+	if (inputElement.value != "") {
+		
+		const li = document.createElement("li");
+		li.textContent = inputElement.value;
+		const deleteBtn = document.createElement("button");
+		deleteBtn.textContent = "❌";
+		deleteBtn.addEventListener("click", function () {
+			listElement.removeChild(li);
+			inputElement.focus();
+		});
+
+
+		li.appendChild(deleteBtn);
+		listElement.appendChild(li);
+
+		inputElement.value = "";
+	}
+    
+	inputElement.focus();
+});
